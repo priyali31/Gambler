b0aaa87c706fc8b6986d1495a0d73ff07cd23c53
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-06-09 10:59:19 +0530

    Find the Lucky and Unlucky day

--- gamblingsimulation.sh
+++ gamblingsimulation.sh
@@ -1,45 +1,66 @@
 #!/bin/bash -x
-
 echo "gambling simulator"
 
 INITIAL_STAKE=100
-BET=1
-DAY=1
 WIN=1
 LOSS=0
+BET=1
 
-stake=$INITIAL_STAKE
 stakePercentAmount=$(( 50*$INITIAL_STAKE/100 ))
-max_win=$(( $stakePercentAmount+$INITIAL_STAKE ))
-max_loss=$(( $INITIAL_STAKE-$stakePercentAmount ))
-numofDays=20
+maxWin=$(( $stakePercentAmount+$INITIAL_STAKE ))
+maxLoss=$(( $INITIAL_STAKE-$stakePercentAmount ))
+numofDays=30
+totalWinOrLoss=0
+daysWin=0
+daysLoss=0
 
-function result() 
-{	stake=$INITIAL_STAKE
-        while [ $stake -lt $max_win ] && [ $stake -gt $max_loss ]
+declare -A fullDay
+declare -A maxMinDay
+function betResult()
+{       dayStake=$INITIAL_STAKE
+        while [ $dayStake -lt $maxWin ] && [ $dayStake -gt $maxLoss ]
         do
-        rem=$(( RANDOM%2 ))
-
-         if [ $rem -eq 1 ]
-         then
-                stake=$(( stake+BET ))
-         else
-                stake=$(( stake-BET ))
-         fi
+         rem=$(( RANDOM % 2 ))
+                if [ $rem -eq 1 ]
+                then
+                        dayStake=$(( $dayStake+$BET ))
+                else
+                        dayStake=$(( $dayStake-$BET ))
+                fi
         done
 }
-
-for (( day=0; day<$numofDays; day++ ))
+        for (( day=1; day<=$numofDays; day++ ))
         do
-        result
-                if [ $stake -eq $max_loss ]
+        betResult
+                if [ $dayStake -eq $maxLoss ]
                 then
-                totalWinOrloss=$(( totalWinOrloss-50 ))
+                       totalWinOrloss=$(( $totalWinOrloss - $stakePercentAmount ))
+                        fullDay["Day $day"]=-$stakePercentAmount
+                        maxMinDay["Day $day"]=$totalWinOrloss
+ 			(( daysLoss++ ))
                 else
-                totalWinOrloss=$(( totalWinOrloss+50 ))
-                fi 
-        echo "Resign for day"
+                        totalWinOrloss=$(( $totalWinOrloss + $stakePercentAmount ))
+                        fullDay["Day $day"]=$stakePercentAmount
+                        maxMinDay["Day $day"]=$totalWinOrloss
+			(( daysWin++ ))
+                fi
         done
 
- echo $totalWinOrloss
+echo "Total Won/loss : $totalWinOrLoss"
+echo "Winned days $daysWin by $(($daysWin*$stakePercentAmount))" 
+echo "Lossed days $daysLoss by  $(($daysLoss*$stakePercentAmount))"
+echo "${!maxMinDay[@]} : ${maxMinDay[@]}"
+luckyDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | head -1 )
+unluckyDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | tail -1 )
 
+
+	for data in "${!maxMinDay[@]}"
+     	do
+		if [[ ${maxMinDay[$data]} -eq $luckyDay ]]
+		then
+		echo "Lucky Day-" $data
+		elif [[ ${maxMinDay[$data]} -eq $unluckyDay ]]
+		then
+		echo "Unlucky Day-" $data
+		fi
+     	done
